485d4f2a697692ce2ab0d6e91d2099155d361b1c
vishalyadav5451 <vishalyadav5451@gmail.com>
2025-09-20 01:20:22 +0530

    Time: 0 ms (100%), Space: 8.6 MB (78.11%) - LeetHub

--- 0258-add-digits/0258-add-digits.cpp
+++ 0258-add-digits/0258-add-digits.cpp
@@ -1,27 +1,14 @@
 class Solution {
 public:
     
-    int sum = 0;
-    int CountOfDigit( int num ) {
-        int count = 0;
-        sum = 0;
-        while ( num != 0) {
-            //num=38
-            sum += num%10; //8
-            num /= 10;
-            count++;
-        }
-
-        return count ;
-    }
-
     int addDigits(int num) {
         
-        while(CountOfDigit(num) > 1 ) {
-
-            num = sum;
-        }
-
-        return sum;
+        if(num == 0) 
+            return 0;
+        
+        if ( num % 9 == 0) 
+            return 9;
+        
+        return num%9 ;
     }
 };
